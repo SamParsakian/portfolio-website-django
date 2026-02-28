249a3e95ee5d4608675b47f532d0f25dab40ef2c
samuel.parsakian@yh.a <samkian0007@gmail.com>
2026-02-28 21:33:27 +0100

    feat: add data-driven site content in site_data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .site_data import SITE
 
-# Create your views here.
+def home(request):
+    ctx = {
+        "profile": SITE["profile"],
+        "social": SITE["social"],
+        "about_paragraphs": SITE["about"],
+        "experience": SITE["experience"],
+        "projects": SITE["projects"],
+        "writing": SITE["writing"],
+    }
+    return render(request, "core/home.html", ctx)
+
+def archive(request):
+    return render(request, "core/archive.html", {
+        "profile": SITE["profile"],
+        "rows": SITE["archive_rows"]
+    })
+
+def articles_archive(request):
+    return render(request, "core/articles_archive.html", {
+        "profile": SITE["profile"],
+        "writing": SITE["writing"],
+    })
